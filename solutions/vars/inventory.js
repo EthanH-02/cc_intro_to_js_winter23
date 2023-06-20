7eeb1033bd63332e85f76bcce1482cb83ff88a76
Ethan <87554637+EthanH-02@users.noreply.github.com>
2023-06-20 19:06:17 +1000

    Create inventory.js

---
+++ solutions/vars/inventory.js
@@ -0,0 +1,18 @@
+/*  TASK:   Initialise an array containing the following elements in order
+                - apple
+                - endura carrot
+                - hearty truffle
+            After initialising the list create another element at the end
+                - mighty banana
+            Following this print each element in order one by one
+*/
+
+
+let inventory = ['apple', 'endura carrot', 'hearty truffle']
+
+inventory[3] = inventory[2]
+
+console.log(inventory[0])
+console.log(inventory[1])
+console.log(inventory[2])
+console.log(inventory[3])
