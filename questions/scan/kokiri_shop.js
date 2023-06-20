ba9a70c3b5f5a0a3f5a800d2ca4a69a15f2f26aa
Ethan <87554637+EthanH-02@users.noreply.github.com>
2023-06-20 19:12:24 +1000

    Create kokiri_shop.js

---
+++ questions/scan/kokiri_shop.js
@@ -0,0 +1,22 @@
+const prompt = require('prompt-sync')();
+
+console.log('Welcome to Kokiri Shop.')
+console.log('How many arrows do you want?')
+
+///////////////////////////////////////////////////////////
+// DO NOT EDIT ABOVE HERE //
+///////////////////////////////////////////////////////////
+
+/*  TASK:   Implement code that takes user input and then prints the number of
+            again and then print the following response. For reference 1 arrow
+            will cost 2 Rupees.
+
+    For n arrows, that will cost x Rupees.
+
+    CHALLENGE:  Kokiri Shop only sells Rupees in bundles of 10 each costing 20
+                Rupees. Research solutions or ask a tutor for help
+
+    For n arrows, you will have to buy m arrows, that will cost x Rupees.
+*/
+
+
