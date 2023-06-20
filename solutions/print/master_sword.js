bcbea4fb3346cee8862df7f5d3d379c93813ce05
Ethan <87554637+EthanH-02@users.noreply.github.com>
2023-06-20 17:08:22 +1000

    Create master_sword.js

---
+++ solutions/print/master_sword.js
@@ -0,0 +1,9 @@
+/*  TASK: Print the following symbol to the terminal:
+              /| _______________
+        o|===|* >_______________>
+              \|
+*/
+
+console.log('      /| _______________')
+console.log('o|===|* >_______________>')
+console.log('      \\|')
