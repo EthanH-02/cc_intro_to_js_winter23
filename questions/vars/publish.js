0167311d139beefd99e1d8d1400869eb95a416b0
Ethan <87554637+EthanH-02@users.noreply.github.com>
2023-06-20 18:59:12 +1000

    Create publish.js

---
+++ questions/vars/publish.js
@@ -0,0 +1,9 @@
+/*  TASK:   Without deleting any from the line below print the following string
+            accurately using. You can use Wikipedia to find the date specified.
+            
+            Your solution should implement both:
+                - A string concatination for the date
+                - An addition for the year
+*/
+
+
