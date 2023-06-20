db92435fe2b7cbfe89c0ba0c1d1b4a3d301c469e
Ethan <87554637+EthanH-02@users.noreply.github.com>
2023-06-20 17:09:42 +1000

    Create cuccoo.js

---
+++ solutions/print/cuccoo.js
@@ -0,0 +1,9 @@
+/*  TASK: Print the following symbol to the terminal:
+        >0__/   \__0<
+          \/     \/
+          ""     ""
+*/
+
+console.log('>0__/   \\__0<')
+console.log('  \\/     \\/')
+console.log('  ""     ""')
