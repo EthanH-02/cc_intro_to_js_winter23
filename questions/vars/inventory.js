a29cdcf0a568af94fb1ea036bc778a2a879d69ca
Ethan <87554637+EthanH-02@users.noreply.github.com>
2023-06-20 19:07:25 +1000

    Create inventory.js

---
+++ questions/vars/inventory.js
@@ -0,0 +1,10 @@
+/*  TASK:   Initialise an array containing the following elements in order
+                - apple
+                - endura carrot
+                - hearty truffle
+            After initialising the list create another element at the end
+                - mighty banana
+            Following this print each element in order one by one
+*/
+
+
