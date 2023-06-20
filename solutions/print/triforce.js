3add2003ea1272cf97190b27fb34a3bef3b1c3e7
Ethan <87554637+EthanH-02@users.noreply.github.com>
2023-06-20 17:07:50 +1000

    Implemented the solutions for triforce.js

---
+++ solutions/print/triforce.js
@@ -0,0 +1,11 @@
+/*  TASK: Print the following symbol to the terminal
+           /\
+          /__\
+         /\  /\
+        /__\/__\
+*/
+
+console.log('   /\\')
+console.log('  /__\\')
+console.log(' /\\  /\\')
+console.log('/__\\/__\\')
