f6d96923ee15165b6d57e61ba94f02b87bd187f3
Ethan <87554637+EthanH-02@users.noreply.github.com>
2023-06-23 22:58:34 +1000

    Create pick_up.js

---
+++ questions/if/pick_up.js
@@ -0,0 +1,27 @@
+const prompt = require('prompt-sync')();
+
+const item = prompt('Which item has Link acquired: '))
+
+///////////////////////////////////////////////////////////
+// DO NOT EDIT ABOVE HERE //
+///////////////////////////////////////////////////////////
+
+/*  TASK:   Depending on the value of the string 'item' print the following
+
+            'item' is 'Master Sword'
+    Link acqiured the legendary Master Sword!
+
+            'item' is 'Hylian Shield'
+    Link acquired the unbreakable Hylian Shield
+
+            'item' is 'Bow'
+    Link acquired the powerful Bow
+
+            'item' is 'Bomb'
+    Link acquired a set of Bombs
+
+            Otherwise print the following
+    No such item exists in Hyrule
+*/
+
+
