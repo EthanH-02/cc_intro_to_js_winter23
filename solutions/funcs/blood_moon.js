b128d9571a858d3b1697a3db45d0685ce6419975
Ethan <87554637+EthanH-02@users.noreply.github.com>
2023-06-23 23:32:42 +1000

    Create blood_moon.js

---
+++ solutions/funcs/blood_moon.js
@@ -0,0 +1,36 @@
+/*  TASK:   Implement a function that takes a given integer parameter and if it
+            is divisible by 7 print
+
+    Witness the blood moon's rise.
+
+            Then implement a loop from 1 to 25 print
+    
+    Day NUM
+
+            Then call the function with the day value
+
+            The output should look like:
+    Day 1
+    Day 2
+    Day 3
+    Day 4
+    Day 5
+    Day 6
+    Day 7
+        Witness the blood moon's rise.
+    Day 8
+    Day 9
+    Day 10
+    ...
+*/
+
+function bloodMoon(day) {
+    if (day % 7 == 0) {
+        console.log('\tWitness the blood moon\'s rise.')
+    }
+}
+
+for (let i = 1 ; i <= 25 ; i++) {
+    console.log(`Day ${i}`)
+    bloodMoon(i)
+}
