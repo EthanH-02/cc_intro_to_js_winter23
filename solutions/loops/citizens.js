e39bb58bd9b290f95e66f039b8fae246a9ea807f
Ethan <87554637+EthanH-02@users.noreply.github.com>
2023-06-23 23:10:01 +1000

    Create citizens.js

---
+++ solutions/loops/citizens.js
@@ -0,0 +1,18 @@
+const prompt = require('prompt-sync')();
+
+const citizens = ['Goron', 'Zora', 'Rito', 'Gerudo', 'Zonai', 'Hylian']
+console.log('In Hyrule you will find:')
+
+///////////////////////////////////////////////////////////
+// DO NOT EDIT ABOVE HERE //
+///////////////////////////////////////////////////////////
+
+/*  TASK:   The program has been given a list called 'citizens' from here print
+            out the list of all the values stored in citizens to the terminal
+            along with this print it in the format
+    - citizen
+*/
+
+for (let citizen in citizens) {
+    console.log(`\t- ${citizens[citizen]}`)
+}
