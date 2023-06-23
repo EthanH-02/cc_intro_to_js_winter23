7d1e2275d1b02b6defe46855e906b70ab65c3c3d
Ethan <87554637+EthanH-02@users.noreply.github.com>
2023-06-23 22:42:26 +1000

    Create legend.js

---
+++ solutions/scan/legend.js
@@ -0,0 +1,20 @@
+const prompt = require('prompt-sync')();
+
+///////////////////////////////////////////////////////////
+// DO NOT EDIT ABOVE HERE //
+///////////////////////////////////////////////////////////
+
+/*  TASK:   Print the following messages however before it prints each line
+            require the user to press the 'enter' key before printing the next
+            line.
+    
+    In the vast, deep forest of Hyrule...
+    Long have I served as the guardian spirit...
+    I am known as the Deku Tree...
+*/
+
+console.log('In the vast, deep forest of Hyrule...')
+prompt()
+console.log('Long have I served as the guardian spirit...')
+prompt()
+console.log('I am known as the Deku Tree...')
