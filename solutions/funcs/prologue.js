53d297aae977fd9b3be0a233cb4fb7ba4d8365c6
Ethan <87554637+EthanH-02@users.noreply.github.com>
2023-06-23 23:15:17 +1000

    Create prologue.js

---
+++ solutions/funcs/prologue.js
@@ -0,0 +1,17 @@
+/*  TASK:   Define a function that contains a series of print statements from
+            below. From here call the function.
+
+    Many years ago prince
+    darkness " gannon " stole
+    one of the trifoce with
+    power.
+*/
+
+function prologue() {
+    console.log('Many years ago prince')
+    console.log('darkness " gannon " stole')
+    console.log('one of the trifoce with')
+    console.log('power.')
+}
+
+prologue()
