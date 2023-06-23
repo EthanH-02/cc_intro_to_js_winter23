335ba2428de0766fb3fc6089a94855e9fa894085
Ethan <87554637+EthanH-02@users.noreply.github.com>
2023-06-23 23:36:46 +1000

    Create requiem.js

---
+++ solutions/funcs/requiem.js
@@ -0,0 +1,25 @@
+/*  TASK:   Write a function called 'isWindReq'. Within the function if the
+            value passed in is '^<>' return true otherwise return false
+*/
+
+function isWindReq(song) {
+    if (song = '^<>') {
+        return true
+    }
+    return false
+}
+
+///////////////////////////////////////////////////////////
+// DO NOT EDIT BELOW HERE //
+///////////////////////////////////////////////////////////
+
+let windReq = '^<>'
+let balladGales = 'v><^'
+
+if (isWindReq(windReq)) {
+    console.log('You conducted the Wind\'s Requiem!')
+}
+
+if (isWindReq(balladGales)) {
+    console.log('That song does not exist.')
+}
