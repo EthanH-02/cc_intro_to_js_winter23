3bea19dfbb8f5380153bda52d1312462c63c600a
Ethan <87554637+EthanH-02@users.noreply.github.com>
2023-06-23 23:06:15 +1000

    Create health.js

---
+++ questions/loops/health.js
@@ -0,0 +1,20 @@
+const prompt = require('prompt-sync')();
+
+console.log('Welcome to Goron Hot Springs, rest here to recover health')
+
+///////////////////////////////////////////////////////////
+// DO NOT EDIT ABOVE HERE //
+///////////////////////////////////////////////////////////
+
+/*  TASK:   Prompt the user for Link's current health with the following
+            message
+    
+    What is Link's current HP: 
+
+            After storing that value with a loop count up by ones printing as
+            the loop goes, until it reaches the maximum value of 40
+    
+    Link's health: curr_hp
+*/
+
+
