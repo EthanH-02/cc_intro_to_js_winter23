cefc1e711dcf55a86aa1223e7d1237433a0b7ca2
Ethan <87554637+EthanH-02@users.noreply.github.com>
2023-06-23 22:53:09 +1000

    Create heart.js

---
+++ questions/if/heart.js
@@ -0,0 +1,19 @@
+const prompt = require('prompt-sync')();
+
+const num_pieces = Number(prompt('How many heart pieces does Link have? '))
+
+///////////////////////////////////////////////////////////
+// DO NOT EDIT ABOVE HERE //
+///////////////////////////////////////////////////////////
+
+/*  TASK:   If the number of the variable 'heart_pieces' is greater than or
+            equal to 4 print the following message
+    
+    Your life has increased by one and is now fully replenished!
+
+            Otherwise find the number of pieces left and print the following
+    
+    Collect four to increase your heart capacity by one. NUM_LEFT to go!
+*/
+
+
