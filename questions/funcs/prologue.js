0b04c57dd0e2111be013d08ba7b41505a9dd08e1
Ethan <87554637+EthanH-02@users.noreply.github.com>
2023-06-23 23:15:42 +1000

    Create prologue.js

---
+++ questions/funcs/prologue.js
@@ -0,0 +1,10 @@
+/*  TASK:   Define a function that contains a series of print statements from
+            below. From here call the function.
+
+    Many years ago prince
+    darkness " gannon " stole
+    one of the trifoce with
+    power.
+*/
+
+
