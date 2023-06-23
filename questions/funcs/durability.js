c4647f3d78cbcf84aa626ae3709f0dc27d4de66a
Ethan <87554637+EthanH-02@users.noreply.github.com>
2023-06-23 23:21:28 +1000

    Create durability.js

---
+++ questions/funcs/durability.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+
+///////////////////////////////////////////////////////////
+// DO NOT EDIT ABOVE HERE //
+///////////////////////////////////////////////////////////
+
+/*  TASK:   Create a function that calculates the durability left of a shield
+            by prompting for the shield's durability, then attack damage of an
+            attack. Following this return the difference between the durability
+            and damage.
+
+            After completing this print a message stating the return value of
+            the attack.
+*/
+
+
